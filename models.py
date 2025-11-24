b4ca9965b2a339ee4dd02b8c9fa3a125842070f2
oGustavoProgrammer <gustavo.lbsilva10@gmail.com>
2025-11-24 17:41:19 -0300

    Criação do arquivo "models.py" USANDO SQLITE COMO DB
    Estou usando o sqlite apenas para fins de teste, assim que o código estiver funcionando eu mudarei para o PostgreSQL.

---
+++ models.py
@@ -0,0 +1,64 @@
+from sqlalchemy import create_engine, Column, String, Integer, ForeignKey, Boolean
+from sqlalchemy.orm import declarative_base
+
+# criação do database.
+db = create_engine('sqlite:///database.db')
+
+# criação da base do database.
+base = declarative_base()
+
+# declaração das classes.
+class Pessoa(base):
+    __tablename__ = 'Pessoas'
+
+    id = Column('ID', Integer, primary_key=True, autoincrement=True)
+    juridica = Column('Jurídica', Boolean, nullable=False, default=False)
+    estado = Column('Estado', String, nullable=False)
+    municipio = Column('Município', String, nullable=False)
+    endereco = Column('Endereço', String, nullable=False)
+    email = Column('E-Mail', String, nullable=False)
+    senha = Column('Senha', String, nullable=False)
+    whatsapp = Column('Whatsapp', Integer)
+    website = Column('Website', String)
+
+    def __init__(self, juridica, estado, municipio, endereco, email, senha, whatsapp, website):
+        self.juridica = juridica
+        self.estado = estado
+        self.municipio = municipio
+        self.endereco = endereco
+        self.email = email
+        self.senha = senha
+        self.whatsapp = whatsapp
+        self.website = website
+
+class PessoaJuridica(Pessoa):
+    __tablename__ = 'Pessoas Jurídicas'
+
+    id = Column('ID', Integer, primary_key=True, autoincrement=True)
+    cnpj = Column('CNPJ', Integer, nullable=False)
+    pessoa_id = Column('Pessoa_ID', ForeignKey('Pessoas.ID'))
+    nome_replegal = Column('Nome_Representante_Legal', String, nullable=False)
+    cpf_replegal = Column('CPF_Representante_Legal', Integer, nullable=False)
+    nome_fantasia = Column('Nome_Fantasia', String)
+
+    def __init__(self, juridica, estado, municipio, endereco, email, senha, whatsapp, website, cnpj, pessoa_id, nome_replegal, cpf_replegal, nome_fantasia):
+        super().__init__(juridica, estado, municipio, endereco, email, senha, whatsapp, website)
+        self.cnpj = cnpj
+        self.pessoa_id = pessoa_id
+        self.nome_replegal = nome_replegal
+        self.cpf_replegal = cpf_replegal
+        self.nome_fantasia = nome_fantasia
+
+class PessoaFisica(Pessoa):
+    __tablename__ = 'Pessoas Físicas'
+
+    id = Column('ID', Integer, primary_key=True, autoincrement=True)
+    cpf = Column('CPF', Integer, nullable=False)
+    pessoa_id = Column('Pessoa_ID', Integer, ForeignKey('Pessoas.ID'))
+    nome = Column('Nome', String, nullable=False)
+
+    def __init__(self, juridica, estado, municipio, endereco, email, senha, whatsapp, website, cpf, pessoa_id, nome):
+        super().__init__(juridica, estado, municipio, endereco, email, senha, whatsapp, website)
+        self.cpf = cpf
+        self.pessoa_id = pessoa_id
+        self.nome = nome
