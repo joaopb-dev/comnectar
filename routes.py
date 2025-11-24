9811aa06ab37b3663e51c58e1cc9b87cc5ec5472
oGustavoProgrammer <gustavo.lbsilva10@gmail.com>
2025-11-24 17:39:27 -0300

    Refazendo o caminho "routes.py" (INCOMPLETO)
    Refactor user registration logic and remove unused routes.

--- routes.py
+++ routes.py
@@ -1,24 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from models import Pessoa, PessoaFisica, PessoaJuridica
+from dependencies import pegar_session
+from main import bcrypt_context
+from schemas import PessoaSchema, PessoaFisicaSchema, PessoaJuridicaSchema
+from sqlalchemy.orm import Session
 
 # roteador do app no arquivo main.py definindo o prefixo do endpoint como auth e a tag do docs como auth também.
 auth_router = APIRouter(prefix='/auth', tags= ['auth'])
 
 # rota de cadastro.
-@auth_router.get('/register')
-async def register_page():
-    ...
-
-# confirmar cadastro.
 @auth_router.post('/register')
-async def register():
-    ...
-
-# rota de login.
-@auth_router.get('/login')
-async def login_page():
-    ...
-
-# confirmar login.
-@auth_router.post('/login')
-async def login():
-    ...
+def new_user(
+    pessoa_schema: PessoaSchema,
+    pessoaj_schema: PessoaJuridicaSchema,
+    pessoaf_schema: PessoaFisicaSchema,
+    session: Session = Depends(pegar_session)
+    ):
+    pessoa = session.query(Pessoa).filter(Pessoa).filter(Pessoa.email == pessoa_schema.email).first
+    if pessoa:
+        raise HTTPException(status_code=400, detail='E-mail de usuário já cadastrado.')
+    else:
+        senha_criptografada = bcrypt_context.hash(pessoa_schema.senha)
